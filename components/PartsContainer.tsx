13a9e383d4fc57bfa3a5974d5bd310f449227cc6
MagoMihaly <mihaly.mago1@gmail.com>
2024-07-10 21:46:45 +0200

    parts

--- components/PartsContainer.tsx
+++ components/PartsContainer.tsx
@@ -20,7 +20,7 @@ export function PartsContainer() {
 
       {partsList.map((card, i) => (
         <div key={i}>
-          <CardContainer className="flex gap-1">
+          <CardContainer className="flex flex-wrap gap-2 w-90 ">
             <CardBody className="relative group/card h-auto hover:shadow-2xl hover:shadow-emerald-500/[0.1] border-white/[0.2] rounded-lg border bg-[#10132E]">
               <CardItem
                 translateZ="50"
@@ -31,15 +31,15 @@ export function PartsContainer() {
               <CardItem
                 as="p"
                 translateZ="60"
-                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 p-2"
+                className="text-neutral-500 text-sm max-w-sm mt-1 dark:text-neutral-300 p-2"
               >
                 {card.description}
               </CardItem>
-              <CardItem translateZ="100" className="w-full mt-4">
+              <CardItem translateZ="100" className="w-full mt-3">
                 <Image
                   src={card.img}
-                  height="300"
-                  width="300"
+                  height="200"
+                  width="200"
                   className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
                   alt="thumbnail"
                 />
